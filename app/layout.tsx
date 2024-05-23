74814461efc571af597401a1cd4b4eccb085277d
gateremark <mark5gatere@gmail.com>
2024-05-23 13:19:12 +0300

    feat: Add open graph image and favicon to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,16 @@ export const metadata: Metadata = {
   title: "DeKUT HACK",
   description:
     "DeKUT HACK is a hackathon event that brings together students from DeKUT to solve real-world problems.",
+  openGraph: {
+    images: [
+      {
+        url: "https://res.cloudinary.com/dvuazircp/image/upload/v1716459487/dekut-hack2_lkunrk.webp",
+      },
+    ],
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
