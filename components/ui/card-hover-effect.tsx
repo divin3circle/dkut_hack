b57062745eb9435704f3d454eed3a7566bba992a
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-05-28 16:11:16 +0300

    Update CardTitle className to include text-center in card-hover-effect.tsx

--- components/ui/card-hover-effect.tsx
+++ components/ui/card-hover-effect.tsx
@@ -92,7 +92,12 @@ export const CardTitle = ({
   children: React.ReactNode;
 }) => {
   return (
-    <h4 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>
+    <h4
+      className={cn(
+        "text-zinc-100 font-bold tracking-wide mt-4 text-center",
+        className
+      )}
+    >
       {children}
     </h4>
   );
